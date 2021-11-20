25c6cf3ee3db670e1411a95dd1d2e54233d8e423
Vlad Ivan <ivanvlad1997@gmail.com>
2021-11-20 21:54:58 +0200

    feat(database): first migration

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Models.Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
