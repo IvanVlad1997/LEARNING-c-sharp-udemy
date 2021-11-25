23734f98a9ec055243a2ec78e0e34681060dee72
Vlad Ivan <ivanvlad1997@gmail.com>
2021-11-25 21:23:45 +0200

    refactor(BulkyBook): Use n-tier architecture

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,5 +1,5 @@
-﻿using BulkyBookWeb.Data;
-using BulkyBookWeb.Models;
+﻿using BulkyBook.DataAccess;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -14,7 +14,7 @@ namespace BulkyBookWeb.Controllers
         }
         public IActionResult Index()
         {
-            IEnumerable<Models.Category> objCategoryList = _db.Categories;
+            IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
 
