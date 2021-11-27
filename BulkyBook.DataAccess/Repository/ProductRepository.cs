159f60e499f5ccc5059e9ea10d0d0a062a5bab5c
Vlad Ivan <ivanvlad1997@gmail.com>
2021-11-27 12:09:26 +0200

    feat(product): add product model

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDBContext _db;
+
+        public ProductRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;    
+                objFromDb.Price = obj.Price;    
+                objFromDb.Category = obj.Category;  
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.ISBN = obj.ISBN;
+                
+            }
+        }
+    }
+}
